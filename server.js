81d59ae839d791afe3fc467218fdf4c8d9ed1632
A-Rehman01 <arehmana377@gmail.com>
2021-08-26 13:38:11 +0500

    Initial commit

---
+++ server.js
@@ -0,0 +1,41 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import colors from 'colors';
+import morgan from 'morgan';
+import cors from 'cors';
+
+//DB
+import connectDB from './config/db.js';
+
+dotenv.config();
+
+//Conneect DB
+connectDB();
+
+const app = express();
+
+//Except JSON data in body-parser
+app.use(express.json());
+
+// Allow Cors
+app.use(cors({ origin: true }));
+
+//APIs info
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+//Test Server
+app.get('/', (req, res) => {
+  res.send('API  is Running...');
+});
+
+//PORT
+const PORT = process.env.PORT || 5000;
+
+app.listen(
+  PORT,
+  console.log(
+    `Server Running ${process.env.NODE_ENV} mode in ${PORT}`.yellow.bold
+  )
+);
